dcd636ee9c976d3d298e22dcb540caf6f0e347b8
lijo123ferdinand <lijothbs@gmail.com>
2024-03-15 11:57:23 +0530

    added redirect to login after delete user

--- tracker/src/components/DeleteUserAccount.js
+++ tracker/src/components/DeleteUserAccount.js
@@ -2,8 +2,11 @@ import React from 'react';
 import { RiDeleteBin2Line } from 'react-icons/ri'; // Importing trash icon
 import axios from 'axios';
 import { jwtDecode } from 'jwt-decode';
+import { useNavigate } from 'react-router-dom'; // Importing useNavigate hook
+
 
 function DeleteUserAccount() {
+  const navigate = useNavigate();
   const handleDeleteUserAccount = async () => {
     if (window.confirm('Are you sure you want to delete your account?')) {
       try {
@@ -17,6 +20,7 @@ function DeleteUserAccount() {
         });
         alert('Your account has been deleted successfully.');
         console.log('Response:', response.data); // Print response body
+        navigate('/login');
       } catch (error) {
         console.error('Error deleting user account:', error);
         alert('Error deleting user account.');
