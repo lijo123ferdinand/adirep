dbadcc6b8c6507c22b9201ba33c9808c1e066097
lijo123ferdinand <lijothbs@gmail.com>
2024-03-18 16:54:47 +0530

    analysis

--- tracker/src/pages/AdminAnalyticsPage.js
+++ tracker/src/pages/AdminAnalyticsPage.js
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom'; // Import Link component
 function AdminAnalyticsPage() {
   return (
     <div>
-        <Link to="/admin">Go to Admin Analytics</Link>
+        {/* <Link to="/admin">Go to Admin Analytics</Link> */}
         <h2>Admin Dashboard</h2>
         <UserChart />
 
--- tracker/src/pages/AdminDashboardPage.js
+++ tracker/src/pages/AdminDashboardPage.js
@@ -7,10 +7,10 @@ function AdminDashboardPage() {
     <div>
       <h2>Admin Dashboard</h2>
       <div className="row">
-        <div className="col-md-8" style={{ maxHeight: 'calc(100vh - 80px)', overflowY: 'auto' }}> {/* UserList takes 60% of the page width */}
+        <div className="col-md-6" style={{ maxHeight: 'calc(100vh - 80px)', overflowY: 'auto' }}> {/* UserList takes 60% of the page width */}
           <UserList />
         </div>
-        <div className="col-md-4"> {/* UserChart takes 40% of the page width */}
+        <div className="col-md-5"> {/* UserChart takes 40% of the page width */}
           <UserChart />
         </div>
       </div>
