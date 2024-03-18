a5e40c74d8f81b58e7508f3f90c75fa43f5131e8
lijo123ferdinand <lijothbs@gmail.com>
2024-03-18 11:28:06 +0530

    css

--- tracker/src/pages/SignupPage.js
+++ tracker/src/pages/SignupPage.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import { Link, useNavigate } from 'react-router-dom';
+import "../styles/SignupPage.css"
 
 function SignupPage() {
   const [email, setEmail] = useState('');
